10b9fc14af8974a06e281a863729c484fa71f935
Andre Santos <andresantoss0598@gmail.com>
2024-07-30 23:42:01 -0300

    finished configuration part

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import Head from "next/head";
+import Image from "next/image"
+import type { Metadata } from "next";
+import logoimg from '../public/logo.svg'
+import styles from './styles/page.module.scss'
+import { Input } from "./compnents/ui";
+
 
 export default function Home() {
+
   return (
-    <div>
-      <h1>Olá</h1>
+    <>
+     <Head>
+        <title>Sujeito Pizza - Faça seu Login</title>
+      </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoimg} alt="logo sujeito pizzaria"/>
+
+      <div className={styles.login}>
+        <form>
+            <Input />
+        </form>
+        </div>
     </div>
-  );
-}
+    </>
+)}
