49f80681464f61ba3b6f6d6dd3e989b42314908a
Andre Santos <andresantoss0598@gmail.com>
2024-08-16 10:26:43 -0300

    Validating token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
+import { GetCookiesServer } from "./lib/cookieServe";
+import { api } from "./service/api";
 
-export async function middleware(){
+export async function middleware(req: NextRequest){
     
-    
+    const { pathname } = req.nextUrl
+    if(pathname.startsWith('/_next') || pathname === '/' ){
+        
+        return NextResponse.next()
 
-    return NextResponse.next()
+    }
+
+    const token = GetCookiesServer()
+    if(pathname.startsWith("/dashboard")){
+        if(!token){
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+
+
+    }
+}
+
+async function vaidateToken(token: string){
+    if(!token) return false
+
+    try{
+
+        await api.get('/me', {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        return true
+
+    }catch(err){
+        return false
+    }
 }
